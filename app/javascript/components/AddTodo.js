5dbcf9ff60731bb4ad27aefbb99cf0dbefa958b1
haryu <hryau6@gmail.com>
2021-10-03 12:20:55 +0900

    add: app.js

---
+++ app/javascript/components/AddTodo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddTodo() {
+  return (
+    <div>
+      Add Tod
+    </div>
+  )
+}
+
+export default AddTodo
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
 import App from '../components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDom.render(
+  ReactDOM.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>,
     document.querySelector('#root'),
-  )
-})
+  );
+});
